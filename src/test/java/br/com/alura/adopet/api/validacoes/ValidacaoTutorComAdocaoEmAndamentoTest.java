288d04fe073ef11be257a0f2523d86146db41dca
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-05-31 15:48:49 -0300

    test: created test case for ValidacaoTutorComAdocaoEmAndamento validator

--- src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComAdocaoEmAndamentoTest.java
+++ src/test/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComAdocaoEmAndamentoTest.java
@@ -2,11 +2,8 @@ package br.com.alura.adopet.api.validacoes;
 
 import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.exception.ValidacaoException;
-import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.StatusAdocao;
-import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
-import br.com.alura.adopet.api.repository.TutorRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,10 +11,6 @@ import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -28,34 +21,16 @@ class ValidacaoTutorComAdocaoEmAndamentoTest {
     @Mock
     private AdocaoRepository adocaoRepository;
     @Mock
-    private TutorRepository tutorRepository;
-    @Mock
-    private Tutor tutor;
-    @Mock
-    private Adocao adocao;
-    @Mock
     private SolicitacaoAdocaoDto dto;
-
-    @Test
-    @DisplayName("Deve permitir que adoção seja realizada")
-    void cenarioPositivo() {
-        BDDMockito.given(adocaoRepository.findAll()).willReturn(Collections.emptyList());
-        BDDMockito.given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
-
-        assertDoesNotThrow(() -> validacao.validar(dto));
-    }
 
     @Test
     @DisplayName("Deveria não permitir e avisar que o tutor já possui outra adoção em andamento")
     void cenarioNegativo() {
         String expectedMessage = "Tutor já possui outra adoção aguardando avaliação!";
-        List<Adocao> adocaoList = new ArrayList<>();
-        adocaoList.add(adocao);
-        adocaoList.add(adocao);
-        BDDMockito.given(adocaoRepository.findAll()).willReturn(adocaoList);
-        BDDMockito.given(adocao.getTutor()).willReturn(tutor);
-        BDDMockito.given(adocao.getStatus()).willReturn(StatusAdocao.AGUARDANDO_AVALIACAO);
-        BDDMockito.given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+
+        BDDMockito.given(
+                adocaoRepository.existsByTutorIdAndStatus(dto.idPet(), StatusAdocao.AGUARDANDO_AVALIACAO))
+                .willReturn(true);
 
         ValidacaoException ex = assertThrows(ValidacaoException.class, () -> validacao.validar(dto));
 
